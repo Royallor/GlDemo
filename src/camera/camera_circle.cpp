640117166938cbfd3943225ed83e878ed4b940fa
sunzhijie <sunzhijie@momenta.ai>
2022-02-08 16:02:16 +0800

    :sparkles:add triangle

--- src/camera/camera_circle.cpp
+++ src/camera/camera_circle.cpp
@@ -282,5 +282,21 @@ int main()
     // load image, create texture and generate mipmaps
     int width, height, nrChannels;
     stbi_set_flip_vertically_on_load(true); // tell stb_image.h to flip loaded texture's on the y-axis
-    unsigned char *data = stbi_load(FileSystem::getPath(""));
+    unsigned char *data = stbi_load(FileSystem::getPath("resources/textures/container.jpg").c_str(),&width,&height,&nrChannels,0);
+    if(data){
+        glTexImage2D(GL_TEXTURE_2D,0,GL_RGB,width,height,0,GL_RGB,GL_UNSIGNED_BYTE,data);
+        glGenerateMipmap(GL_TEXTURE_2D);
+    }
+    else{
+        std::cout<<"Failed to load texture." <<std::endl;
+    }
+    stbi_image_free(data);
+    //texture 2
+    //-----------
+    glGenTextures(1,&texture2);
+    glBindTexture(GL_TEXTURE_2D,texture2);
+    // set the texture wrapping parameters
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_REPEAT);
+    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
 }
